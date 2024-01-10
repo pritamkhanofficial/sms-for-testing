5ed1df8d4bca841886057186d91943dac38ed4c1
Ayesa Khatun <ayesabuie388@gmail.com>
2024-01-11 02:08:43 +0530

    template setup

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -4,8 +4,11 @@ namespace App\Controllers;
 
 class Home extends BaseController
 {
-    public function index(): string
+    public function index()
     {
-        return view('welcome_message');
+        echo view('includes/header');
+        echo view('includes/sidebar');
+        echo view('index');
+        echo view('includes/footer');
     }
 }
---
+++ app/Views/includes/sidebar.php
@@ -0,0 +1,27 @@
+ <!-- ========== Left Sidebar Start ========== -->
+ <div class="vertical-menu">
+
+<div data-simplebar class="h-100">
+
+    <!--- Sidemenu -->
+    <div id="sidebar-menu">
+        <!-- Left Menu Start -->
+        <ul class="metismenu list-unstyled" id="side-menu">
+            <li class="menu-title" key="t-menu">Menu</li>
+
+            <li>
+                <a href="javascript: void(0);" class="has-arrow waves-effect">
+                    <i class="bx bx-home-circle"></i>
+                    <span key="t-dashboards">Dashboards</span>
+                </a>
+                <ul class="sub-menu" aria-expanded="false">
+                    <li><a href="index.html" key="t-default">Dashboard</a></li>
+                </ul>
+            </li>
+
+        </ul>
+    </div>
+    <!-- Sidebar -->
+</div>
+</div>
+<!-- Left Sidebar End -->
